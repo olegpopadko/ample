2b1ca037357273fef2b711c55573059ef07d7884
Oleg Popadko <oleg.popadko@icloud.com>
2015-08-12 16:08:18 +0300

    Add bootstrap and menu

---
+++ src/AppBundle/Menu/Builder.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by Oleg Popadko
+ * Date: 8/12/15
+ * Time: 3:28 PM
+ */
+
+namespace AppBundle\Menu;
+
+use Knp\Menu\FactoryInterface;
+use Symfony\Component\DependencyInjection\ContainerAware;
+
+class Builder extends ContainerAware
+{
+    public function mainMenu(FactoryInterface $factory, array $options)
+    {
+        $menu = $factory->createItem('root');
+
+        $menu->addChild('Home', ['route' => 'homepage']);
+
+        return $menu;
+    }
+}
